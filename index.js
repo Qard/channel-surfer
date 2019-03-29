40e11b54b1829083ffdcbf58e759468316d65332
Stephen Belanger <admin@stephenbelanger.com>
2019-03-29 23:49:44 +0100

    refactor to use double-pointer linked-list (#5)
    By using a linked-list with two moving pointers, much greater operation and memory efficiency can be achieved, This also makes for more predictable behaviour when one side of the channel is outrunning the other.

--- index.js
+++ index.js
@@ -1,108 +1,81 @@
-const Promise = require('any-promise')
+'use strict'
 
-function defer() {
-  let resolve
-  let reject
+const { deprecate } = require('util')
+const ChannelItem = require('./item')
 
-  const promise = new Promise((_resolve, _reject) => {
-    resolve = _resolve
-    reject = _reject
-  })
-
-  return {
-    promise,
-    resolve,
-    reject
-  }
-}
-
+/**
+ * The channel class uses a single-direction linked-list
+ * with two pointers to track the position of input and
+ * output cursors in a theoretically infinite sequence.
+ * By shifting the pointers, any event that has had both
+ * input and output completions will drop out of scope
+ * and get garbage collected automatically.
+ *
+ * Because the functional behaviour of a channel is a
+ * race between two sides, the internal linked-list will
+ * always cover only the span between the input and output
+ * pointers. This design enables optimal memory usage for
+ * a lossless stream.
+ *
+ * To finalize the sequence in a way that mirrors the
+ * behaviour of iterators, an error or close event will
+ * create an item in the list which forms a circular
+ * reference to itself. By doing this, future requests
+ * beyond the end of the sequence will just repeatedly
+ * return the final event indicating the done state.
+ */
 class Channel {
-  constructor() {
-    this.inputs = []
-    this.requests = []
-    this.final = null
+  constructor () {
+    this.input = new ChannelItem()
+    this.request = this.input
   }
 
-  give(value) {
-    let deferred
-    if (this.requests.length) {
-      deferred = this.requests.shift()
-    } else {
-      deferred = defer()
-      this.inputs.push(deferred)
-    }
-
-    deferred.resolve({
-      value,
-      done: false
-    })
-  }
-
-  take() {
-    if (this.final) {
-      return this.final.promise
-    }
-
-    let deferred
-    if (this.inputs.length) {
-      deferred = this.inputs.shift()
-    } else {
-      deferred = defer()
-      this.requests.push(deferred)
-    }
-
-    return deferred.promise
-  }
-
-  giveBack(value) {
-    let deferred
-    if (this.requests.length) {
-      deferred = this.requests.shift()
-    } else {
-      deferred = defer()
-      this.inputs.unshift(deferred)
-    }
-
-    deferred.resolve({
-      value,
-      done: false
-    })
-  }
-
-  error(err) {
-    const deferred = defer()
-    deferred.reject(err)
-    this.final = deferred
-
-    let request
-    while (request = this.requests.shift()) {
-      request.reject(err)
-    }
-  }
-
-  close(value) {
-    const data = {
-      value: value,
-      done: true
-    }
-
-    const deferred = defer()
-    deferred.resolve(data)
-    this.final = deferred
-
-    let request
-    while (request = this.requests.shift()) {
-      request.resolve(data)
-    }
+  give (value) {
+    const { input } = this
+    this.input = input.ensureNext()
+    input.send(value)
   }
 
   next () {
-    return this.take()
+    const { request } = this
+    this.request = request.ensureNext()
+    return request.promise
   }
 
-  [Symbol.asyncIterator]() {
+  giveBack (value) {
+    const { input, request } = this
+
+    // No pending requests or inputs
+    if (!request.next && !input.next) {
+      return this.give(value)
+    }
+
+    // If there are pending inputs, create a
+    // new request and move the request pointer
+    let item = input
+    if (request.next) {
+      item = request.wrap()
+      this.request = item
+    }
+
+    item.send(value)
+  }
+
+  error (error) {
+    this.input.error(error)
+  }
+
+  close () {
+    this.input.close()
+  }
+
+  [Symbol.asyncIterator] () {
     return this
   }
 }
+
+Channel.prototype.take = deprecate(function take () {
+  return this.next()
+}, 'The channel.take() function is deprecated, please use channel.next() or for await loops.')
 
 module.exports = Channel
